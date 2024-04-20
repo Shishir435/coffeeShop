addf8978171c47ee273ffbc71d1a7c433e1c3855
Shishir435 <shishirchaurasiya435@gmail.com>
2024-04-20 23:12:06 +0530

    add tab nav

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {StyleSheet} from 'react-native';
+import DetailsScreen from './src/screens/DetsailsScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+import TabNavigator from './src/navigators/TabNavigator';
 
+const stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <stack.Navigator screenOptions={{headerShown: false}}>
+        <stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}
+        />
+        <stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{animation: 'slide_from_bottom'}}
+        />
+        <stack.Screen
+          name="Payments"
+          component={PaymentScreen}
+          options={{animation: 'slide_from_bottom'}}
+        />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 };
 
